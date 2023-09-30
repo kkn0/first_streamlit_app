18a7a0060d612846d415c2633b92ddd035c1d887
Karthikeyan Karnan <114054340+kkn0@users.noreply.github.com>
2023-09-30 13:22:29 +0530

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ st.text('🥑🍞 Avocado toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas ad pd
+import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
